57d7e4cceaf813f1db97a7b5b502e3c5b1666fdd
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2021-11-02 14:47:16 +0200

    Update Folllow API doc

--- Backend/app/Http/Controllers/FollowController.php
+++ Backend/app/Http/Controllers/FollowController.php
@@ -24,6 +24,16 @@ class FollowController extends Controller
  *    @OA\Schema(
  *       type="integer",
  *       format="int"
+ *    ),
+ *  @OA\Parameter(
+ *    description="ID of Following Blog ",
+ *    in="path",
+ *    name="blog_id",
+ *    required=true,
+ *    example="1",
+ *    @OA\Schema(
+ *       type="integer",
+ *       format="int"
  *    )
  * ),
  * @OA\Response(
@@ -31,14 +41,16 @@ class FollowController extends Controller
  *    description="Successful credentials response",
  *    @OA\JsonContent(
  *       @OA\Property(property="meta", type="object", example={"status": "200", "msg":"ok"}),
- *       @OA\Property(property="response", type="object", example={"follower_id": "1", "msg":"You Follow Ahmed Successfully"})
  *        )
  *     ),
  *  @OA\Response(
  *    response=404,
  *    description="Wrong credentials response",
  *    @OA\JsonContent(
- *       @OA\Property(property="meta", type="object", example={"status": "404", "msg":"error"})
+ *       @OA\Property(property="meta", type="object", example={"status": "404", "msg":"error"}),
+ *       @OA\Property(property="response", type="object", example={"blog_avatar":"/storage/imgname2.extension",
+ *        "blog_avatar_shape":"circle","blog_username":"radwa-ahmed213","blog_id":1032})
+ *       
  *        )
  *     )
  * )
@@ -82,25 +94,60 @@ class FollowController extends Controller
  * @OA\Get(
  * path="/followers",
  * summary="follower's blog",
- * description=" Primary blog  get all followers",
+ * description=" Primary blog  get all his/her followers",
  * operationId="followersblog",
  * tags={"Follow Blogs"},
+ * security={ {"bearer": {} }},
  * @OA\Response(
  *    response=200,
  *    description="Successful credentials response",
  *    @OA\JsonContent(
  *       @OA\Property(property="meta", type="object", example={"status": "200", "msg":"ok"}),
- *       @OA\Property(property="response", type="object", example={"followers": 
- *                    {
- *                      { "blog_id": 1,
- *                        "blog_name":"Radwa"
- *                       } ,
- *                       { "blog_id": 2,
- *                        "blog_name":"Noran"
- *                       } 
- *                   
- *                     }, 
- *               })
+ *       @OA\Property(property="response", type="object",
+ *        @OA\Property(property="followers", type="array",
+ *                  @OA\Items(
+ *                      @OA\Property(property="blog_avatar", type="string", example="/storage/imgname2.extension"),
+ *                      @OA\Property(property="blog_avatar_shape", type="string", example="circle"),
+ *                      @OA\Property(property="blog_username", type="string", example="radwa-ahmed213"),
+ *                      @OA\Property(property="blog_id", type="integer", example=1032),       
+ *                  )
+ *              ),
+ *      )
+ *       )
+ *     ),
+ *  @OA\Response(
+ *    response=404,
+ *    description="Wrong credentials response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="meta", type="object", example={"status": "404", "msg":"error"})
+ *        )
+ *     )
+ * )
+ */
+
+/**
+ * @OA\Get(
+ * path="/followings",
+ * summary="followings's blog",
+ * description=" Primary blog  get all his/her followings",
+ * operationId="followingblog",
+ * tags={"Follow Blogs"},
+ * security={ {"bearer": {} }},
+ * @OA\Response(
+ *    response=200,
+ *    description="Successful credentials response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="meta", type="object", example={"status": "200", "msg":"ok"}),
+ *       @OA\Property(property="response", type="object",        
+ *         @OA\Property(property="followings", type="array",
+ *                  @OA\Items(
+ *                      @OA\Property(property="blog_avatar", type="string", example="/storage/imgname2.extension"),
+ *                      @OA\Property(property="blog_avatar_shape", type="string", example="circle"),
+ *                      @OA\Property(property="blog_username", type="string", example="radwa-ahmed213"),
+ *                      @OA\Property(property="blog_id", type="integer", example=1032),       
+ *                  )
+ *              ),
+ *    )
  *       )
  *     ),
  *  @OA\Response(
@@ -114,27 +161,20 @@ class FollowController extends Controller
  */
 /**
  * @OA\Get(
- * path="/followings",
- * summary="following's blog",
- * description=" Primary blog  get all followings",
+ * path="/followed_by/{id}",
+ * summary="followed_by blog",
+ * description=" Check if followed by blog",
  * operationId="followingblog",
  * tags={"Follow Blogs"},
+ * security={ {"bearer": {} }},
  * @OA\Response(
  *    response=200,
  *    description="Successful credentials response",
  *    @OA\JsonContent(
  *       @OA\Property(property="meta", type="object", example={"status": "200", "msg":"ok"}),
- *       @OA\Property(property="response", type="object", example={"followings": 
- *                    {
- *                      { "blog_id": 1,
- *                        "blog_name":"Radwa"
- *                       } ,
- *                       { "blog_id": 2,
- *                        "blog_name":"Noran"
- *                       } 
- *                   
- *                     }, 
- *               })
+ *         @OA\Property(property="response", type="obeject",example={"followed":true},
+ *                 
+ *              ),
  *       )
  *     ),
  *  @OA\Response(
@@ -146,4 +186,6 @@ class FollowController extends Controller
  *     )
  * )
  */
+
+
 }
