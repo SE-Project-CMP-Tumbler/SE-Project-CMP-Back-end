cf1bc0f2104eeebe61d5d15197ce696d4d26625f
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2021-11-09 21:16:05 +0200

    Refactor DB to use Postgresql

--- Backend/config/database.php
+++ Backend/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mongodb'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
