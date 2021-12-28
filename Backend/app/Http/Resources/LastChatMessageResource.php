ac4ccac3db09ed77de125bf6852b5acaf11c5afc
BigFish2086 <a8686.ibrahim@gmail.com>
2021-12-28 15:52:44 +0200

    fix some chat resources issues

--- Backend/app/Http/Resources/LastChatMessageResource.php
+++ Backend/app/Http/Resources/LastChatMessageResource.php
@@ -22,17 +22,17 @@ class LastChatMessageResource extends JsonResource
             "gif" => $this->gif_url,
             "read" => $this->read,
 
-            "from_blog_username" => $sender->username,
-            "from_blog_id" => $sender->id,
-            "from_blog_avatar" => $sender->avatar,
-            "from_blog_avatar_shape" => $sender->avatar_shape,
-            "from_blog_title" => $sender->title,
+            "blog_username" => $sender->username,
+            "blog_id" => $sender->id,
+            "blog_avatar" => $sender->avatar,
+            "blog_avatar_shape" => $sender->avatar_shape,
+            "blog_title" => $sender->title,
 
-            "friend_blog_username" => $receiver->username,
-            "friend_blog_id" => $receiver->id,
-            "friend_blog_avatar" => $receiver->avatar,
-            "friend_blog_avatar_shape" => $receiver->avatar_shape,
-            "friend_blog_title" => $receiver->title,
+            "friend_username" => $receiver->username,
+            "friend_id" => $receiver->id,
+            "friend_avatar" => $receiver->avatar,
+            "friend_avatar_shape" => $receiver->avatar_shape,
+            "friend_title" => $receiver->title,
         ];
     }
 }
