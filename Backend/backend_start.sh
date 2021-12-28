8820de889e5936db080825fd3d0cb4d30e4f7125
Makrion <miklaziz3112000@gmail.com>
2021-12-28 12:45:13 +0200

    fix-sh
    don't (php artisan passport:install) every time.... it's for first time only
    and this applies for the seed also

--- Backend/backend_start.sh
+++ Backend/backend_start.sh
@@ -6,10 +6,10 @@ php artisan storage:link
 
 php artisan migrate #--seed
 
-php artisan db:seed
+#php artisan db:seed
 
 php artisan l5-swagger:generate
 
-php artisan passport:install
+#php artisan passport:install
 
 php artisan serve --host=0.0.0.0 --port=8181
