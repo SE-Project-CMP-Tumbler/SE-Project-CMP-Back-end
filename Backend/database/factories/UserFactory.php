c50fecb6b891e6a69b96b6b2c97cd66f2cbe6b35
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2021-12-30 23:34:41 +0200

    Update Blog  factory and user factory

--- Backend/database/factories/UserFactory.php
+++ Backend/database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
         $linked_by_google = $this->faker->boolean();
         $google_id = $linked_by_google ? $this->faker->unique()->regexify('[0-9]{21}') : null;
         return [
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => ($this->faker->unique()->firstName) . ($this->faker->unique()->safeEmail()),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'age' => $this->faker->biasedNumberBetween(16, 100),
