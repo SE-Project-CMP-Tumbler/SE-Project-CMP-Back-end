3189121fc55241fecf4cfc74374d3218a77cf2e1
meryacine <omar.groza@gmail.com>
2021-12-31 19:21:41 +0200

    install no more certificates and dont generate api docs

--- Backend/backend_start.sh
+++ Backend/backend_start.sh
@@ -8,8 +8,8 @@ php artisan migrate #--seed
 
 #php artisan db:seed
 
-php artisan l5-swagger:generate
+#php artisan l5-swagger:generate
 
-php artisan passport:install
+#php artisan passport:install
 
 php artisan serve --host=0.0.0.0 --port=8181
