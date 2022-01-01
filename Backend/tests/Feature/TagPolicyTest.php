f7c82e79a0d3f151b7b46e2823046be862d6c88d
NouranHany <hany.noran@yahoo.com>
2022-01-01 16:54:44 +0200

    Unit Test for post, tag, follow tag is added.

--- Backend/tests/Feature/TagPolicyTest.php
+++ Backend/tests/Feature/TagPolicyTest.php
@@ -12,6 +12,8 @@ use Tests\TestCase;
 
 class TagPolicyTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * Testing the authorization to create a tag
      *
@@ -37,5 +39,29 @@ class TagPolicyTest extends TestCase
         $post = Post::factory()->create(['blog_id' => $blog->id]);
         $userGuest = User::factory()->create();
         $this->assertFalse($userGuest->can('create', [Tag::class, $post]));
+    }
+    /**
+     * Testing the authorization of the user to view follow tag relation
+     *
+     * @return void
+     */
+    public function testCanViewFollowRelation()
+    {
+        $user = User::factory()->create();
+        $blog = Blog::factory()->create(['user_id' => $user]);
+        $this->assertTrue($user->can('viewFollowRelation', [Tag::class, $blog]));
+    }
+    /**
+     * Testing the non authorization of the user to view follow tag relation
+     * A user is not allowed to view the follow relation status between a blog he don't own and any tag.
+     *
+     * @return void
+     */
+    public function testCanNotViewFollowRelation()
+    {
+        $user = User::factory()->create();
+        $blog = Blog::factory()->create(['user_id' => $user]);
+        $geustUser = User::factory()->create();
+        $this->assertFalse($geustUser->can('viewFollowRelation', [Tag::class, $blog]));
     }
 }
